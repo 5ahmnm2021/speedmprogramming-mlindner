a681da9ee97b8f9f69cf301b1a50decf2aad885f
yslindner <marco.lindner@htl-salzburg.ac.at>
2021-04-13 08:48:34 +0200

    Creating Folders and SceneManager

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// Scene Manager Marco Lindner
+
+public class sceneLoader : MonoBehaviour
+{
+    public void LoadColorScene()
+    {
+        SceneManager.LoadScene("scene2");
+    }
+
+    public void LoadNumberScene()
+    {
+        SceneManager.LoadScene("scene3");
+    }
+
+}
